6647daf84c41879cba7f2efe20eb8b49f05ef03b
Faisal Wafa <faisalwafa10@gmail.com>
2024-12-22 17:47:34 +0700

    adding button

--- lib/main.dart
+++ lib/main.dart
@@ -1,32 +1,12 @@
+import 'package:first_app/gradient_color.dart';
 import 'package:flutter/material.dart';
 
 void main() {
   runApp(
     MaterialApp(
-      home: Scaffold(body: GradientContainer()),
+      home: Scaffold(
+          body:
+              GradientContainer([Colors.deepPurple, Colors.deepPurpleAccent])),
     ),
   );
-}
-
-class GradientContainer extends StatelessWidget {
-  @override
-  Widget build(context) {
-    return Container(
-      decoration: const BoxDecoration(
-        gradient: LinearGradient(colors: [
-          Color.fromARGB(255, 26, 2, 80),
-          Color.fromARGB(255, 45, 7, 98),
-        ], begin: Alignment.topLeft, end: Alignment.bottomRight),
-      ),
-      child: const Center(
-        child: Text(
-          'Hello World!',
-          style: TextStyle(
-            color: Colors.white,
-            fontSize: 28,
-          ),
-        ),
-      ),
-    );
-  }
 }
